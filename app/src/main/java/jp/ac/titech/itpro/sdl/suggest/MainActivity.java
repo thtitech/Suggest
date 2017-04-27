32c5dbb4f3ea70fb7011b083481fd260526c8aa7
Takuo Watanabe <takuo@acm.org>
2017-04-27 10:53:26 +0900

    First runnable code using asynctask

--- app/src/main/java/jp/ac/titech/itpro/sdl/suggest/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/suggest/MainActivity.java
@@ -2,8 +2,7 @@ package jp.ac.titech.itpro.sdl.suggest;
 
 import android.app.SearchManager;
 import android.content.Intent;
-import android.os.Handler;
-import android.os.Message;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Xml;
@@ -20,12 +19,12 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText inputText;
     private ArrayAdapter<String> resultAdapter;
-    private ArrayList<String> result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,13 +38,12 @@ public class MainActivity extends AppCompatActivity {
         suggestButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                resultAdapter.clear();
-                new SuggestThread(inputText.getText().toString().trim()).start();
+                String queryText = inputText.getText().toString().trim();
+                new SuggestTask().execute(queryText);
             }
         });
 
-        result = new ArrayList<>();
-        resultAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, result);
+        resultAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new ArrayList<String>());
         resultList.setAdapter(resultAdapter);
         resultList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -58,34 +56,14 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private final static int MSG_RESULT = 1111;
-
-    private Handler handler = new Handler(new Handler.Callback() {
+    private class SuggestTask extends AsyncTask<String, Void, List<String>> {
         @Override
-        public boolean handleMessage(Message msg) {
-            switch (msg.what) {
-            case MSG_RESULT:
-                resultAdapter.notifyDataSetChanged();
-                inputText.selectAll();
-                break;
-            }
-            return false;
-        }
-    });
-
-    private class SuggestThread extends Thread {
-        private String queryText;
-
-        SuggestThread(String queryText) {
-            this.queryText = queryText;
-        }
-
-        @Override
-        public void run() {
+        protected List<String> doInBackground(String... params) {
+            ArrayList<String> result = new ArrayList<>();
             HttpURLConnection conn = null;
             String error = null;
             try {
-                String query = URLEncoder.encode(queryText, "UTF-8");
+                String query = URLEncoder.encode(params[0], "UTF-8");
                 URL url = new URL(getString(R.string.suggest_url, query));
                 conn = (HttpURLConnection)url.openConnection();
                 conn.setConnectTimeout(10000);
@@ -115,7 +93,14 @@ public class MainActivity extends AppCompatActivity {
             }
             if (result.size() == 0)
                 result.add(getString(R.string.no_suggestions));
-            handler.sendMessage(handler.obtainMessage(MSG_RESULT));
+            return result;
+        }
+
+        @Override
+        public void onPostExecute(List<String> result) {
+            resultAdapter.clear();
+            resultAdapter.addAll(result);
+            inputText.selectAll();
         }
     }
 }
